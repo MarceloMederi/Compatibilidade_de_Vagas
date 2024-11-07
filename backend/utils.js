a69c8ba3f9a660734f3d4554f0074cc05051f618
MarceloMederi <marcelomederi11@gmail.com>
2024-11-07 10:12:55 -0300

    Alterado a estrutura do utils.js para Remoção do Arquivo PDF após Processamento, Divisão por Zero em totalWords, Tratamento de Erro Específico

--- backend/utils.js
+++ backend/utils.js
@@ -1,26 +1,18 @@
-// utils.js
-
 const pdfParse = require('pdf-parse');
 const fs = require('fs');
-const path = require('path');
 
 const calculateCompatibility = async (file, jobDescription) => {
-    // Lê o arquivo PDF como um buffer
+    // Ler o arquivo PDF como um buffer
     const dataBuffer = fs.readFileSync(file.path);
 
     try {
-        // Analisar o PDF e extrair o texto
+        // Analisar o PDF e extrair texto
         const data = await pdfParse(dataBuffer);
         const resumeText = data.text.toLowerCase();
 
         // Processar a descrição da vaga
         const jobDescriptionWords = jobDescription.toLowerCase().split(/\s+/);
         const totalWords = jobDescriptionWords.length;
-
-        if (totalWords === 0) {
-            return '0,00'; // Retorna 0% de compatibilidade se a descrição da vaga estiver vazia
-        }
-
         let matchedWords = 0;
 
         // Contar as palavras que aparecem no currículo
@@ -34,17 +26,12 @@ const calculateCompatibility = async (file, jobDescription) => {
         const compatibility = (matchedWords / totalWords) * 100;
 
         // Formatar a porcentagem com duas casas decimais
-        const formattedCompatibility = compatibility.toFixed(2).replace('.', ',');
+        const formattedCompatibility = compatibility.toFixed(2).replace('.', ','); // Aqui formatamos a porcentagem
 
         return formattedCompatibility; // Retorna a porcentagem formatada
     } catch (error) {
-        console.error(`Erro ao processar o arquivo PDF (${file.originalname}):`, error);
+        console.error('Erro ao processar o PDF:', error);
         throw error;
-    } finally {
-        // Remover o arquivo PDF após o processamento, independentemente de sucesso ou erro
-        fs.unlink(path.join(__dirname, file.path), (err) => {
-            if (err) console.error('Erro ao excluir o arquivo:', err);
-        });
     }
 };
 
